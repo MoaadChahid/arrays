144563ca3e28e5b369ed6412e53abfabec557828
MoaadChahid <chahidmoaad1@gmail.com>
2025-12-31 17:23:32 +0100

    Ripasso del ciclo if ed else

---
+++ Prove.js
@@ -0,0 +1,22 @@
+let a = 10
+let b = 15
+
+if (a > b ) {
+   console.log(`A è più grande di B, quindi il suo valore è: ${a}`)
+} else {
+    console.log (`B è più grande di A, quindi il suo valore è: ${b}`)
+
+}
+
+let c = 25
+let d = 30
+
+c+= 10
+
+console.log (`Il nuovo valore di C è: ${c}`)
+
+if (c < d ) {
+    console.log (`C è più piccolo di D, quindi il valore di C è: ${c}`)
+    } else {
+        console.log (`D è più piccolo di C, quindi il valore di D è: ${d}`)
+}
