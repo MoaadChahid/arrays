3e1686675a251224d1f77e0449ed95660ab944e6
MoaadChahid <chahidmoaad1@gmail.com>
2025-12-31 17:43:01 +0100

    Ripasso Array

--- Prove.js
+++ Prove.js
@@ -1,3 +1,6 @@
+//if e else
+
+
 let a = 10
 let b = 15
 
@@ -20,3 +23,17 @@ if (c < d ) {
     } else {
         console.log (`D è più piccolo di C, quindi il valore di D è: ${d}`)
 }
+
+
+//ARRAY
+
+let Studenti = `Mario, Luca , Leo, Edoardo e Giulio `
+let Studentese = `Anna, Giulia, Francesca, Martina e Sara `
+
+console.log (`Gli studenti maschi sono: ${Studenti} ed le studentesse sono: ${Studentese}`)
+
+let inegnanti = `Paolo, Marco e Giovanni`
+let bidelli = `Silvia e Laura`
+let preside = `Dott.ssa Rossi`
+
+console.log (`Gli insegnanti Iniziano alle 8.00, è sono: ${inegnanti}, i bidelli iniziano alle 7:45, è sono: ${bidelli} e il preside Inizia alle 9:00 ,e il preside è : ${preside}`)
